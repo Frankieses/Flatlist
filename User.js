188156b8f98f433a6147de504709cf76317f4c9a
Dave Francis C. Baugbog <124759430+Frankieses@users.noreply.github.com>
2023-11-27 20:14:17 +0800

    Add files via upload

---
+++ User.js
@@ -0,0 +1,182 @@
+export const users = [
+  {
+    id: 1,
+    name: {
+      firstname: "Juan",
+      middlename: "Santos",
+      lastname: "Dela Cruz",
+    },
+    address: "Manila, Philippines",
+  },
+  {
+    id: 2,
+    name: {
+      firstname: "Maria",
+      middlename: "T",
+      lastname: "Reyes",
+    },
+    address: "Quezon City, Philippines",
+  },
+  {
+    id: 3,
+    name: {
+      firstname: "Jose",
+      middlename: "Mendoza",
+      lastname: "Gonzales",
+    },
+    address: "Makati, Philippines",
+  },
+  {
+    id: 4,
+    name: {
+      firstname: "Rosa",
+      middlename: "P",
+      lastname: "Santos",
+    },
+    address: "Taguig, Philippines",
+  },
+  {
+    id: 5,
+    name: {
+      firstname: "Ramon",
+      middlename: "B",
+      lastname: "Aquino",
+    },
+    address: "Cebu City, Philippines",
+  },
+  {
+    id: 6,
+    name: {
+      firstname: "Lea",
+      middlename: "T",
+      lastname: "Reyes",
+    },
+    address: "Davao City, Philippines",
+  },
+  {
+    id: 7,
+    name: {
+      firstname: "Antonio",
+      middlename: "L",
+      lastname: "Lopez",
+    },
+    address: "Iloilo City, Philippines",
+  },
+  {
+    id: 8,
+    name: {
+      firstname: "Aurora",
+      middlename: "P",
+      lastname: "Magno",
+    },
+    address: "Cagayan de Oro, Philippines",
+  },
+  {
+    id: 9,
+    name: {
+      firstname: "Rodrigo",
+      middlename: "M",
+      lastname: "Cruz",
+    },
+    address: "Baguio City, Philippines",
+  },
+  {
+    id: 10,
+    name: {
+      firstname: "Luisa",
+      middlename: "G",
+      lastname: "Torres",
+    },
+    address: "Manila, Philippines",
+  },
+  {
+    id: 11,
+    name: {
+      firstname: "Carlos",
+      middlename: "A",
+      lastname: "Villanueva",
+    },
+    address: "Quezon City, Philippines",
+  },
+  {
+    id: 12,
+    name: {
+      firstname: "Isabel",
+      middlename: "S",
+      lastname: "Ramirez",
+    },
+    address: "Makati, Philippines",
+  },
+  {
+    id: 13,
+    name: {
+      firstname: "Fernando",
+      middlename: "R",
+      lastname: "Perez",
+    },
+    address: "Taguig, Philippines",
+  },
+  {
+    id: 14,
+    name: {
+      firstname: "Elena",
+      middlename: "C",
+      lastname: "Gomez",
+    },
+    address: "Cebu City, Philippines",
+  },
+  {
+    id: 15,
+    name: {
+      firstname: "Javier",
+      middlename: "D",
+      lastname: "Hernandez",
+    },
+    address: "Davao City, Philippines",
+  },
+  {
+    id: 16,
+    name: {
+      firstname: "Carmen",
+      middlename: "M",
+      lastname: "Fernandez",
+    },
+    address: "Iloilo City, Philippines",
+  },
+  {
+    id: 17,
+    name: {
+      firstname: "Marcelo",
+      middlename: "P",
+      lastname: "Ramos",
+    },
+    address: "Cagayan de Oro, Philippines",
+  },
+  {
+    id: 18,
+    name: {
+      firstname: "Sofia",
+      middlename: "S",
+      lastname: "Cortez",
+    },
+    address: "Baguio City, Philippines",
+  },
+  {
+    id: 19,
+    name: {
+      firstname: "Alejandro",
+      middlename: "R",
+      lastname: "Santos",
+    },
+    address: "Manila, Philippines",
+  },
+  {
+    id: 20,
+    name: {
+      firstname: "Amelia",
+      middlename: "M",
+      lastname: "Lopez",
+    },
+    address: "Quezon City, Philippines",
+  },
+];
